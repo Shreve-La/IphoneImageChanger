a4832b3251b34c85cf23f27f2dfe2b24160be1b8
Shreve-La <shrevela@gmail.com>
2017-05-22 17:50:10 -0400

    Add Button to change image randomly without repeating.

--- NSurlSession/ViewController.m
+++ NSurlSession/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *iPhoneImageView;
+@property (nonatomic, strong) NSString *lastUrl;
+@property (nonatomic, strong) NSString *randomUrl;
+
 
 @end
 
@@ -16,14 +20,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.lastUrl = @"http://imgur.com/bktnImE.png";
+    NSURL *url = [NSURL URLWithString: self.lastUrl];
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data]; // 2
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // This will run on the main queue
+            
+            self.iPhoneImageView.image = image; // 4
+        }];
+        
+    }];
+                                              
+    
+    [downloadTask resume];
+    
+}
+- (IBAction)randomImage:(id)sender {
+
+    do {
+        NSArray <NSString *> *images = @[@"http://imgur.com/bktnImE.png", @"http://imgur.com/zdwdenZ.png", @"http://imgur.com/CoQ8aNl.png", @"http://imgur.com/2vQtZBb.png", @"http://imgur.com/y9MIaCS.png"];
+    int randNum = arc4random_uniform(5);
+    self.randomUrl = images[randNum];
+    } while ([self.randomUrl isEqualToString:(self.lastUrl)]);
+
+    self.lastUrl = self.randomUrl;
+    
+    NSURL *url = [NSURL URLWithString: self.randomUrl];
+
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data]; // 2
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // This will run on the main queue
+            
+            self.iPhoneImageView.image = image; // 4
+        }];
+        
+    }];
+    
+    
+    [downloadTask resume];
+
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
